bfc14f5db8d2a0134f709d6dfb4f2b9790d5892f
thecreativegang <thecreativegang503@gmail.com>
2022-07-18 17:11:02 +0600

    server added

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 3001;
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+//middle wire
+
+
+app.use(cors())
+app.use(express.json())
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egywk.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        // const collection = client.db("database").collection("collection");
+        // console.log('DB Connected');
+
+
+        //get requests
+        app.get('/', async (req, res) => {
+            res.send("Server Running")
+        });
+
+
+    } catch (error) {
+
+    }
+}
+run().catch(console.dir)
+
+    ;
+
+app.listen(port, async (req, res) => {
+    console.log("Listening to ", port);
+})
