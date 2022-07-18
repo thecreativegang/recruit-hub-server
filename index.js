e074552495f4d0e29dd15c0052802999dd2e99f4
thecreativegang <thecreativegang503@gmail.com>
2022-07-18 17:34:23 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 3001;
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 //middle wire
 
 
 app.use(cors())
 app.use(express.json())
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.egywk.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uofdyvi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         // const collection = client.db("database").collection("collection");
-        // console.log('DB Connected');
+        console.log('DB Connected');
 
 
         //get requests
