156e708f5661b7de97f2398c5d1f8f77f098a166
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-26 18:13:57 +0600

    Adding fake data

--- routes/user.js
+++ routes/user.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../Schemas/')
+const User = require('../Schemas/userSchema');
 
 //Function for token generation
 const generateToken = (userData) => {
-    console.log(userData)
-    return jwt.sign(userData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
-}
+  console.log(userData);
+  return jwt.sign(userData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
+};
 
 router.post('/', async (req, res) => {
-    console.log('hit')
-    const accessToken = generateToken(req.body);
+  console.log('hit');
+  const accessToken = generateToken(req.body);
 
-    res.json({
-        accessToken
-    })
+  res.json({
+    accessToken,
+  });
 });
-
 
 module.exports = router;
