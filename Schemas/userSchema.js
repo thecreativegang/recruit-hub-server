e44478634c0f12a3b88b72f3bbfb788a8b6ce95d
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-08-02 18:08:53 +0600

    merge with test assessment

--- Schemas/userSchema.js
+++ Schemas/userSchema.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema({
   accountType: {
     type: String,
     enum: ['developer', 'recruiter'],
-    default: 'developer'
+    default: ''
   },
   isAdmin: {
     type: Boolean,
