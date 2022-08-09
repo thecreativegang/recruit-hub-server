0e6dd12d4cbeab0d1b753631110d1e81b727af42
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-08-09 16:16:19 +0600

    added res.send in /user/creat

--- controller/userController.js
+++ controller/userController.js
@@ -4,19 +4,18 @@ const { sendError } = require('../utilities/errorHelper');
 const userSchema = require('../Schemas/userSchema');
 const User = new mongoose.model("User", userSchema);
 const jwt = require('jsonwebtoken');
-const { get } = require('../routes/user');
 
 const generateToken = (userData) => {
   return jwt.sign(userData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
 };
 exports.create = async (req, res) => {
   const userData = req.body;
+  console.log(userData)
   const accessToken = await generateToken({ userData });
-
 
   //check if email is already registered or not
   const ifEmailAlreadyRegistered = await User.find({
-    email: userData?.email
+    email: userData.email
   });
 
   //get the user info if registered
@@ -38,6 +37,9 @@ exports.create = async (req, res) => {
         })
       }
     })
+  }
+  else {
+    res.json({ message: "success" })
   }
 };
 
