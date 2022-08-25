0ae73d76b795ca5743784712b46157b5f72f4fc8
SouravBiswas238 <souravbiswas427@gmail.com>
2022-08-25 21:57:10 +0600

    update api

--- routes/user.js
+++ routes/user.js
@@ -11,25 +11,25 @@ const User = require('../Schemas/userSchema');
 
 //JWT Verify
 function verifyJWT(req, res, next) {
-  authHeader = req?.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized Access' });
-  }
-  else {
-    const token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
-      // err
-      if (err) {
-        console.log(err)
-        return res.status(403).send({ message: 'Forbiddendgfdf' });
-      }
-      else {
-        req.decoded = decoded;
-        next();
-        return;
-      }
-    });
-  }
+    authHeader = req?.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized Access' });
+    }
+    else {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.JWT_SECRET_KEY, function (err, decoded) {
+            // err
+            if (err) {
+                console.log(err)
+                return res.status(403).send({ message: 'Forbiddendgfdf' });
+            }
+            else {
+                req.decoded = decoded;
+                next();
+                return;
+            }
+        });
+    }
 }
 
 // console.log('hello')
